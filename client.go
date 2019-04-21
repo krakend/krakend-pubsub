9e3c75b7ab4f3aaab35369def250121acf68a245
kpacha <kpacha666@gmail.com>
2019-04-21 22:50:44 +0200

    export the backend factory

--- client.go
+++ client.go
@@ -1,30 +1,47 @@
 package pubsub
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+
+	"gocloud.dev/pubsub"
+	_ "gocloud.dev/pubsub/awssnssqs"
+	_ "gocloud.dev/pubsub/azuresb"
+	_ "gocloud.dev/pubsub/gcppubsub"
+	_ "gocloud.dev/pubsub/natspubsub"
+	_ "gocloud.dev/pubsub/rabbitpubsub"
 
 	"github.com/devopsfaith/krakend/config"
 	"github.com/devopsfaith/krakend/logging"
 	"github.com/devopsfaith/krakend/proxy"
 )
 
-func NewBackendFactory(ctx context.Context, logger logging.Logger, bf proxy.BackendFactory) proxy.BackendFactory {
-	f := backendFactory{
+var OpenCensusViews = pubsub.OpenCensusViews
+
+const (
+	publisherNamespace  = "github.com/devopsfaith/krakend-pubsub/publisher"
+	subscriberNamespace = "github.com/devopsfaith/krakend-pubsub/subscriber"
+)
+
+func NewBackendFactory(ctx context.Context, logger logging.Logger, bf proxy.BackendFactory) *BackendFactory {
+	return &BackendFactory{
 		logger: logger,
 		bf:     bf,
 		ctx:    ctx,
 	}
-
-	return f.New
 }
 
-type backendFactory struct {
+type BackendFactory struct {
 	ctx    context.Context
 	logger logging.Logger
 	bf     proxy.BackendFactory
 }
 
-func (f backendFactory) New(remote *config.Backend) proxy.Proxy {
+func (f *BackendFactory) New(remote *config.Backend) proxy.Proxy {
 	if prxy, err := f.initSubscriber(f.ctx, remote); err == nil {
 		return prxy
 	}
@@ -34,4 +51,119 @@ func (f backendFactory) New(remote *config.Backend) proxy.Proxy {
 	}
 
 	return f.bf(remote)
+}
+
+func (f *BackendFactory) initPublisher(ctx context.Context, remote *config.Backend) (proxy.Proxy, error) {
+	dns := remote.Host[0]
+	cfg := &publisherCfg{}
+	if err := getConfig(remote, publisherNamespace, cfg); err != nil {
+		f.logger.Debug(fmt.Sprintf("pubsub: publisher (%s): %s", dns, err.Error()))
+		return proxy.NoopProxy, err
+	}
+
+	t, err := pubsub.OpenTopic(ctx, dns+cfg.TopicURL)
+	if err != nil {
+		f.logger.Error(fmt.Sprintf("pubsub: %s", err.Error()))
+		return proxy.NoopProxy, err
+	}
+
+	go func() {
+		<-ctx.Done()
+		t.Shutdown(context.Background())
+	}()
+
+	return func(ctx context.Context, r *proxy.Request) (*proxy.Response, error) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return nil, err
+		}
+		headers := map[string]string{}
+		for k, vs := range r.Headers {
+			headers[k] = vs[0]
+		}
+		msg := &pubsub.Message{
+			Metadata: headers,
+			Body:     body,
+		}
+
+		if err := t.Send(ctx, msg); err != nil {
+			return nil, err
+		}
+		return &proxy.Response{IsComplete: true}, nil
+	}, nil
+}
+
+func (f *BackendFactory) initSubscriber(ctx context.Context, remote *config.Backend) (proxy.Proxy, error) {
+	dns := remote.Host[0]
+	cfg := &subscriberCfg{}
+	if err := getConfig(remote, subscriberNamespace, cfg); err != nil {
+		f.logger.Debug(fmt.Sprintf("pubsub: subscriber (%s): %s", dns, err.Error()))
+		return proxy.NoopProxy, err
+	}
+
+	topicURL := dns + cfg.SubscriptionURL
+
+	sub, err := pubsub.OpenSubscription(ctx, topicURL)
+	if err != nil {
+		f.logger.Error(fmt.Sprintf("pubsub: opening subscription for %s: %s", topicURL, err.Error()))
+		return proxy.NoopProxy, err
+	}
+
+	go func() {
+		<-ctx.Done()
+		sub.Shutdown(context.Background())
+	}()
+
+	ef := proxy.NewEntityFormatter(remote)
+
+	return func(ctx context.Context, _ *proxy.Request) (*proxy.Response, error) {
+		msg, err := sub.Receive(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		var data map[string]interface{}
+		if err := remote.Decoder(bytes.NewBuffer(msg.Body), &data); err != nil && err != io.EOF {
+			// TODO: figure out how to Nack if possible
+			// msg.Nack()
+			return nil, err
+		}
+
+		msg.Ack()
+
+		newResponse := proxy.Response{Data: data, IsComplete: true}
+		newResponse = ef.Format(newResponse)
+		return &newResponse, nil
+	}, nil
+}
+
+type publisherCfg struct {
+	TopicURL string `json:"topic_url"`
+}
+
+type subscriberCfg struct {
+	SubscriptionURL string `json:"subscription_url"`
+}
+
+func getConfig(remote *config.Backend, namespace string, v interface{}) error {
+	cfg, ok := remote.ExtraConfig[namespace]
+	if !ok {
+		return &NamespaceNotFoundErr{
+			Namespace: namespace,
+		}
+	}
+
+	b, err := json.Marshal(&cfg)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, &v)
+}
+
+type NamespaceNotFoundErr struct {
+	Namespace string
+}
+
+func (n *NamespaceNotFoundErr) Error() string {
+	return n.Namespace + " not found in the extra config"
 }
---
+++ client_test.go
@@ -0,0 +1,190 @@
+package pubsub
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"gocloud.dev/pubsub"
+	_ "gocloud.dev/pubsub/mempubsub"
+
+	"github.com/devopsfaith/krakend/config"
+	"github.com/devopsfaith/krakend/encoding"
+	"github.com/devopsfaith/krakend/logging"
+	"github.com/devopsfaith/krakend/proxy"
+)
+
+func TestNew_noConfig(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	called := false
+
+	fallback := func(remote *config.Backend) proxy.Proxy {
+		called = true
+		return proxy.NoopProxy
+	}
+
+	buff := bytes.NewBuffer(make([]byte, 1024))
+	logger, _ := logging.NewLogger("DEBUG", buff, "")
+
+	bf := NewBackendFactory(ctx, logger, fallback)
+
+	prxy := bf.New(&config.Backend{
+		Host: []string{"schema://host"},
+	})
+
+	prxy(context.Background(), &proxy.Request{})
+
+	if !called {
+		t.Error("fallback should be called")
+	}
+
+	lines := strings.Split(buff.String(), "\n")
+	if !strings.HasSuffix(lines[0], "DEBUG: pubsub: subscriber (schema://host): github.com/devopsfaith/krakend-pubsub/subscriber not found in the extra config") {
+		t.Error("unexpected first log line:", lines[0])
+	}
+	if !strings.HasSuffix(lines[1], "DEBUG: pubsub: publisher (schema://host): github.com/devopsfaith/krakend-pubsub/publisher not found in the extra config") {
+		t.Error("unexpected second log line:", lines[1])
+	}
+	if lines[2] != "" {
+		t.Error("unexpected final log line:", lines[2])
+	}
+}
+
+func TestNew_subscriber(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	fallback := func(remote *config.Backend) proxy.Proxy {
+		t.Error("fallback shouldn't be called")
+		return proxy.NoopProxy
+	}
+
+	buff := new(bytes.Buffer)
+	logger, _ := logging.NewLogger("DEBUG", buff, "")
+
+	bf := NewBackendFactory(ctx, logger, fallback)
+
+	topic, err := pubsub.OpenTopic(ctx, "mem://host/subscriber-topic-url")
+
+	prxy := bf.New(&config.Backend{
+		Host:    []string{"mem://host"},
+		Decoder: encoding.JSONDecoder,
+		ExtraConfig: config.ExtraConfig{
+			subscriberNamespace: &subscriberCfg{
+				SubscriptionURL: "/subscriber-topic-url",
+			},
+		},
+	})
+
+	topic.Send(ctx, &pubsub.Message{
+		Body: []byte(`{"foobar":42}`),
+	})
+
+	resp, err := prxy(context.Background(), &proxy.Request{})
+
+	if err != nil && err != context.Canceled {
+		t.Error(err)
+	}
+
+	if log := buff.String(); log != "" {
+		t.Errorf("unexpected log: '%s'", log)
+	}
+
+	if !resp.IsComplete {
+		t.Errorf("got an incomplete response")
+		return
+	}
+
+	v := resp.Data["foobar"]
+	foobar, ok := v.(json.Number)
+	if !ok {
+		t.Errorf("unexpected response: %+v", resp.Data)
+		return
+	}
+	if foobar.String() != "42" {
+		t.Errorf("unexpected response: %+v", resp.Data)
+		return
+	}
+}
+
+func TestNew_publisher(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	fallback := func(remote *config.Backend) proxy.Proxy {
+		t.Error("fallback shouldn't be called")
+		return proxy.NoopProxy
+	}
+
+	buff := bytes.NewBuffer(make([]byte, 1024))
+	logger, _ := logging.NewLogger("DEBUG", buff, "")
+
+	bf := NewBackendFactory(ctx, logger, fallback)
+
+	prxy := bf.New(&config.Backend{
+		Host: []string{"mem://host"},
+		ExtraConfig: config.ExtraConfig{
+			publisherNamespace: &publisherCfg{
+				TopicURL: "/publisher-topic-url",
+			},
+		},
+	})
+
+	prxy(context.Background(), &proxy.Request{Body: ioutil.NopCloser(bytes.NewBufferString(`{"foo":"bar"}`))})
+
+	lines := strings.Split(buff.String(), "\n")
+	if !strings.HasSuffix(lines[0], "DEBUG: pubsub: subscriber (mem://host): github.com/devopsfaith/krakend-pubsub/subscriber not found in the extra config") {
+		t.Error("unexpected first log line:", lines[0])
+	}
+	if lines[1] != "" {
+		t.Error("unexpected final log line:", lines[1])
+	}
+}
+
+func TestNew_publisher_unknownProvider(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	called := false
+
+	fallback := func(remote *config.Backend) proxy.Proxy {
+		called = true
+		return proxy.NoopProxy
+	}
+
+	buff := bytes.NewBuffer(make([]byte, 1024))
+	logger, _ := logging.NewLogger("DEBUG", buff, "")
+
+	bf := NewBackendFactory(ctx, logger, fallback)
+
+	prxy := bf.New(&config.Backend{
+		Host: []string{"schema://host"},
+		ExtraConfig: config.ExtraConfig{
+			publisherNamespace: &publisherCfg{
+				TopicURL: "/publisher-topic-url",
+			},
+		},
+	})
+
+	prxy(context.Background(), &proxy.Request{Body: ioutil.NopCloser(bytes.NewBufferString(`{"foo":"bar"}`))})
+
+	if !called {
+		t.Error("fallback should be called")
+	}
+
+	lines := strings.Split(buff.String(), "\n")
+	if !strings.HasSuffix(lines[0], "DEBUG: pubsub: subscriber (schema://host): github.com/devopsfaith/krakend-pubsub/subscriber not found in the extra config") {
+		t.Error("unexpected first log line:", lines[0])
+	}
+	if !strings.HasSuffix(lines[1], `ERROR: pubsub: open pubsub.Topic: no provider registered for "schema" for URL "schema://host/publisher-topic-url"; available schemes: awssnssqs, azuresb, gcppubsub, mem, nats, rabbit`) {
+		t.Error("unexpected second log line:", lines[1])
+	}
+	if lines[2] != "" {
+		t.Error("unexpected final log line:", lines[2])
+	}
+}
