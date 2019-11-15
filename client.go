2d2574cf871a606cbc22384b2be6274045f69761
kpacha <kpacha666@gmail.com>
2019-11-15 17:58:40 +0100

    gocloud upgraded to 0.18.0

--- client.go
+++ client.go
@@ -12,6 +12,7 @@ import (
 	_ "gocloud.dev/pubsub/awssnssqs"
 	_ "gocloud.dev/pubsub/azuresb"
 	_ "gocloud.dev/pubsub/gcppubsub"
+	_ "gocloud.dev/pubsub/kafkapubsub"
 	_ "gocloud.dev/pubsub/natspubsub"
 	_ "gocloud.dev/pubsub/rabbitpubsub"
 
